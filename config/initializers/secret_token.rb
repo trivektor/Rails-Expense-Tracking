02dc9ee7c461110aa469d4d96f97c63cd5d6fa3c
trivektor <trivektor@gmail.com>
2012-09-29 11:39:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsExpenseTracking::Application.config.secret_token = '1bc97ec300f9163938cbd412c7530dd90b427beab5a5024cf6ef178cac4f8495423a272336bdece7339352bb12fa52b1a10684e12b2dfc076802b81874bd57a1'
