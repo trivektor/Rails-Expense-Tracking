5987bb52ab5d8f6525ea79ad3f31e60de06b6015
trivektor <trivektor@gmail.com>
2012-09-29 17:58:49 -0700

    Override devise's registrations_controller#create

---
+++ test/functional/devise/registrations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Devise::RegistrationsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/helpers/devise/registrations_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Devise::RegistrationsHelperTest < ActionView::TestCase
+end
