320d80eb25761536539d32364e21bdcc2b61afb9
trivektor <trivektor@gmail.com>
2012-10-20 18:53:12 -0700

    add expenses#destroy

--- app/controllers/api/expenses_controller.rb
+++ app/controllers/api/expenses_controller.rb
@@ -1,11 +1,10 @@
 class Api::ExpensesController < ApplicationController
   
   before_filter :find_user
+  before_filter :find_expense, :only => [:show, :destroy]
   
   def index
-    respond_to do |format|
-      format.json { render :json => Expense.all }
-    end
+    render :json => {:expenses => @user.expenses.latest_first}
   end
   
   def create
@@ -17,10 +16,26 @@ class Api::ExpensesController < ApplicationController
     end
   end
   
+  def show
+    render :json => {:expense => @expense}
+  end
+  
+  def destroy
+    if @expense.destroy
+      render :json => {:success => 1, :message => 'Expense has been deleted'}
+    else
+      render :json => {:success => -1, :message => 'An error has occurred'}
+    end
+  end
+  
   private
   
   def find_user
     @user = User.find_by_authentication_token(params[:token])
+  end
+  
+  def find_expense
+    @expense = @user.expenses.find(params[:id])
   end
   
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,4 +4,16 @@ class Expense < ActiveRecord::Base
   validates_presence_of :name
   validates :amount, :numericality => {:only_integer => false}
   
+  scope :latest_first, order('created_at DESC')
+  
+  def as_json(options={})
+    {
+      :id => self.id,
+      :name => self.name,
+      :amount => self.amount.to_f,
+      :created_at => self.created_at.in_time_zone('Pacific Time (US & Canada)').strftime("%d/%m/%Y %H:%M%P"),
+      :description => self.description || ""
+    }
+  end
+  
 end
