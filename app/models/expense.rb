32146d5a578750aa62799077209308dbff71c48c
trivektor <trivektor@gmail.com>
2012-10-20 19:59:20 -0700

    update as_json of Expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,16 +3,21 @@ class Expense < ActiveRecord::Base
   
   validates_presence_of :name
   validates :amount, :numericality => {:only_integer => false}
+  validates :tax, :numericality => {:only_integer => false}, :if => Proc.new { |e| !e.tax.blank? }
+  validates :tip, :numericality => {:only_integer => false}, :if => Proc.new { |e| !e.tip.blank? }
   
   scope :latest_first, order('created_at DESC')
   
   def as_json(options={})
     {
-      :id => self.id,
-      :name => self.name,
-      :amount => self.amount.to_f,
-      :created_at => self.created_at.in_time_zone('Pacific Time (US & Canada)').strftime("%d/%m/%Y %H:%M%P"),
-      :description => self.description || ""
+      :id           => self.id,
+      :name         => self.name,
+      :amount       => self.amount.to_f,
+      :tax          => self.tax.to_f,
+      :tip          => self.tip.to_f,
+      :category     => self.category || "n/a",
+      :created_at   => self.created_at.in_time_zone('Pacific Time (US & Canada)').strftime("%d/%m/%Y %H:%M%P"),
+      :description  => self.description || ""
     }
   end
   
