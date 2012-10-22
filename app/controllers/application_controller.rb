b0daee9e36724a5269a18621c39f285c60d52d00
trivektor <trivektor@gmail.com>
2012-10-21 21:13:08 -0700

    styling signup and signin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   skip_before_filter  :verify_authenticity_token
-  before_filter :adjust_format_for_iphone
+  include Mobylette::RespondToMobileRequests
+  #before_filter :adjust_format_for_iphone
   
-  private
-  
-  def adjust_format_for_iphone
-    request.format = :ios if ios_user_agent?
-  end
-
-  def ios_user_agent?
-    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
-  end
+  # private
+  # 
+  # def adjust_format_for_iphone
+  #   request.format = :ios if ios_user_agent?
+  # end
+  # 
+  # def ios_user_agent?
+  #   request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
+  # end
   
 end
