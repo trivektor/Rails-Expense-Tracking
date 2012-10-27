54d24d41c2d6ad1d08d741cc8a4ff3ae8e729ba4
trivektor <trivektor@gmail.com>
2012-10-27 00:01:26 -0700

    issue auth_token upon successful login

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,11 @@
-
 class DashboardController < ApplicationController
   
+  before_filter :authenticate_user!
+  
   def index
+    # Revisit
+    current_user.ensure_authentication_token!
+    cookies[:auth_token] = current_user.authentication_token
   end
   
 end
