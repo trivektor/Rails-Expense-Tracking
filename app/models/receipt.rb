9e55d04c6f5143ef2b742bff014a92f11bd2f779
trivektor <trivektor@gmail.com>
2012-11-11 21:14:38 -0800

    update attr_accessible for image

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,12 +1,19 @@
 class Receipt < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :image
+  
   has_attached_file :image
+  attr_accessible :name,
+                  :description,
+                  :image_file_name, 
+                  :image_content_type, 
+                  :image_file_size, 
+                  :image_updated_at
+  
   
   scope :latest_first, order('created_at DESC')
   
   before_save do
-    self.name = 'Expense' if self.name.blank?
+    self.name = 'Receipt' if self.name.blank?
   end
   
 end
