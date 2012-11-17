501a22d1f35f23760598a70f74c8dc70506acdb5
trivektor <trivektor@gmail.com>
2012-11-17 14:16:46 -0800

    add medium and thumbnail receipt images

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -2,10 +2,14 @@ class Receipt < ActiveRecord::Base
   # attr_accessible :title, :body
   
   has_attached_file :image,
+    :styles => {
+      :medium => "300x300>",
+      :thumb  => "100x100>"
+    },
     :storage => :aws,
     :s3_credentials => {
-      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+      :access_key_id      => ENV['S3_ACCESS_KEY_ID'],
+      :secret_access_key  => ENV['S3_SECRET_ACCESS_KEY']
     },
     :s3_acl => :public_read,
     :s3_protocol => 'http',
@@ -24,10 +28,12 @@ class Receipt < ActiveRecord::Base
   
   def as_json(options={})
     {
-      :name         => self.name,
-      :image_url    => self.image.url,
-      :description  => self.description,
-      :created_at   => self.created_at.in_time_zone('Pacific Time (US & Canada)').strftime("%d/%m/%Y %H:%M%P"),
+      :name             => self.name,
+      :full_image_url   => self.image.url,
+      :medium_image_url => self.image.url(:medium),
+      :thumb_image_url  => self.image.url(:thumb),
+      :description      => self.description,
+      :created_at       => self.created_at.in_time_zone('Pacific Time (US & Canada)').strftime("%d/%m/%Y %H:%M%P"),
     }
   end
   
