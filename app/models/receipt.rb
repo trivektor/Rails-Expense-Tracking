8b24aa241d23ca5225453e30d2b6549fe8cc7626
trivektor <trivektor@gmail.com>
2012-11-17 12:03:35 -0800

    add s3 config

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,12 +1,25 @@
 class Receipt < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :image
-  has_attached_file :image
+  
+  has_attached_file :image,
+    :storage => :aws,
+    :s3_credentials => {
+      :access_key_id => env['S3_ACCESS_KEY_ID'],
+      :secret_access_key => env['S3_SECRET_ACCESS_KEY']
+    },
+    :s3_acl => :public_read,
+    :s3_protocol => 'http',
+    :s3_bucket => :expense_tracker,
+    :path => "receipts/:id/:style/:filename"
+    
+  attr_accessible :name,
+                  :description,
+                  :image  
   
   scope :latest_first, order('created_at DESC')
   
   before_save do
-    self.name = 'Expense' if self.name.blank?
+    self.name = 'Receipt' if self.name.blank?
   end
   
 end
